b6a1580c682752f3d51af2af929a428c08164c8c
jiro.kirameki <ec2-user@ip-172-31-18-27.us-east-2.compute.internal>
2018-06-08 04:52:49 +0000

    post

--- app/User.php
+++ app/User.php
@@ -7,6 +7,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
+     public function microposts()
+    {
+        return $this->hasMany(Micropost::class);
+    }
     use Notifiable;
 
     /**
@@ -27,3 +31,6 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 }
+
+// 
+?>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,9 @@ Route::get('/', function () {
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+Route::get('/', 'MicropostsController@index');
 Route::group(['middleware' => ['auth']], function () {
 Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
