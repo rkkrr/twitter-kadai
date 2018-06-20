308e1bfded2737a14cc2f057b6a70ccf910e0b16
jiro.kirameki <ec2-user@ip-172-31-18-27.us-east-2.compute.internal>
2018-06-20 00:35:36 +0000

    follow, unfollow

---
+++ app/Http/Controllers/UserFollowController.php
@@ -0,0 +1,14 @@
+class UserFollowController extends Controller
+{
+    public function store(Request $request, $id)
+    {
+        \Auth::user()->follow($id);
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        \Auth::user()->unfollow($id);
+        return redirect()->back();
+    }
+}
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -12,8 +12,10 @@
             </div>
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
+                    <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
+                    <li><a href="#">Login</a></li>
+                </ul>
                     @if (Auth::check())
-                    <li>{!! link_to_route('users.index', 'Users') !!}</li>
                         <li><a href="#">Users</a></li>
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
